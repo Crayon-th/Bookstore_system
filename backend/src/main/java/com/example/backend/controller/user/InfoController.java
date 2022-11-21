c7cac787e8194f0a871b58e7df239c650d9ced68
Crayon <snail.cray@gmail.com>
2022-11-21 16:41:05 +0800

    get user info

---
+++ backend/src/main/java/com/example/backend/controller/user/InfoController.java
@@ -0,0 +1,21 @@
+package com.example.backend.controller.user;
+
+import com.example.backend.service.User.InfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class InfoController {
+
+    @Autowired
+    private InfoService infoService;
+
+    @GetMapping("/user/userinfo")
+    public Map<String,String> getuserinfo(){
+        return infoService.getuserinfo();
+    }
+}
---
+++ backend/src/main/java/com/example/backend/service/User/InfoService.java
@@ -0,0 +1,7 @@
+package com.example.backend.service.User;
+
+import java.util.Map;
+
+public interface InfoService {
+    Map<String,String> getuserinfo();
+}
