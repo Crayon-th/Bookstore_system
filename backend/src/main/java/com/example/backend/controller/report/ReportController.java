8d114500a59408b25ab2038f6d8d536011e1deba
Why Not <83454355+skl523@users.noreply.github.com>
2022-11-27 00:09:24 +0800

    Add files via upload

---
+++ backend/src/main/java/com/example/backend/controller/report/ReportController.java
@@ -0,0 +1,29 @@
+package com.example.backend.controller.report;
+
+import com.example.backend.pojo.ViolationReport;
+import com.example.backend.service.report.ReportService;
+import com.example.backend.utils.picUtil;
+import com.example.backend.utils.timeUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@RestController
+public class ReportController {
+    @Autowired
+    private ReportService reportService;
+    private timeUtil timeUtil=new timeUtil();
+    @PostMapping("/user/report/")
+    public int Report(int reportID, int violationType, String violationDescription, MultipartFile evidence, HttpServletRequest request) throws IOException {
+        picUtil picUtil=new picUtil();
+        String evidencePath=picUtil.savePicture(evidence,"report",request);
+        String date=timeUtil.getCurrentTimeStamp();
+        ViolationReport violationReport=new ViolationReport(null,reportID,violationType,violationDescription,evidencePath,date);
+        System.out.println(evidencePath);
+        return reportService.Report(violationReport);
+    }
+}
