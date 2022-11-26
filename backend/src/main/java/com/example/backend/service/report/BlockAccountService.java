fc34d5b1d559d43f654c1d692fe7792c61560315
Why Not <83454355+skl523@users.noreply.github.com>
2022-11-26 23:54:34 +0800

    6、8部分更新

---
+++ backend/src/main/java/com/example/backend/service/report/BlockAccountService.java
@@ -0,0 +1,5 @@
+package com.example.backend.service.report;
+
+public interface BlockAccountService {
+    int blockAccount(int userID);
+}
---
+++ backend/src/main/java/com/example/backend/service/report/ReportService.java
@@ -0,0 +1,7 @@
+package com.example.backend.service.report;
+
+import com.example.backend.pojo.ViolationReport;
+
+public interface ReportService {
+    int Report(ViolationReport violationReport);
+}
