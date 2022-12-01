61f32204175417c7068ea66076488014222c340b
Why Not <83454355+skl523@users.noreply.github.com>
2022-12-01 16:50:49 +0800

    增加授权

--- backend/src/main/java/com/example/backend/config/WebSecurityConfig.java
+++ backend/src/main/java/com/example/backend/config/WebSecurityConfig.java
@@ -105,7 +105,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 
                 //允许匿名及登录用户访问
-                .antMatchers("/api/auth/**", "/error/**","/api/admin/**").permitAll()
+                .antMatchers("/api/auth/**", "/error/**","/api/admin/**","/admin/**","/user/**").permitAll()
                 // 所有请求都需要认证
                 .anyRequest().authenticated()
                 .and().apply(securityConfigurerAdapter());
